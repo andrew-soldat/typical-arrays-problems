11b5d720820623b0f1d789bacfa19afc4f75b36b
andrew-soldat <blishch.andrew@gmail.com>
2020-10-02 09:18:07 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    array.sort((a, b) => a - b);
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    array.sort((a, b) => a - b).reverse();
+    return array[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array === undefined || array.length === 0) return 0;
+	let sum = 0;
+	for (let element of array) {
+		sum += element;
+	}
+	return sum / array.length;
 }
