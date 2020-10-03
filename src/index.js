d6cc76a42b2d7ef91ef70d89b465a25270c633a3
andrew-soldat <blishch.andrew@gmail.com>
2020-10-03 11:27:50 +0300

    improved the algorithm

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
-
-exports.min = function min (array) {
+exports.min = function min(array) {
     if (array === undefined || array.length === 0) return 0;
-    array.sort((a, b) => a - b);
-    return array[0];
-}
+    return Math.min(...array);
+};
 
-exports.max = function max (array) {
+exports.max = function max(array) {
     if (array === undefined || array.length === 0) return 0;
-    array.sort((a, b) => a - b).reverse();
-    return array[0];
-}
+    return Math.max(...array);
+};
 
-exports.avg = function avg (array) {
-	if (array === undefined || array.length === 0) return 0;
-	let sum = 0;
-	for (let element of array) {
-		sum += element;
-	}
-	return sum / array.length;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) return 0;
+    return array.reduce((acc, cur) => acc + cur) / array.length;
+};
